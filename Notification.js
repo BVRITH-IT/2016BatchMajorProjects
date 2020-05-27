3aa7c491801dfdaed6af51af9304c3bafc77efd1
soundaryakasarla <35767882+soundaryakasarla@users.noreply.github.com>
2020-05-27 11:23:06 +0530

    Delete Notification.js

--- Notification.js
+++
@@ -1,9 +0,0 @@
-var button = document.createElement("button");
-button.innerHTML = "Do Something";
-
-var body = document.getElementsByTagName("body")[0];
-body.appendChild(button);
-
-button.addEventListener ("Notify me", function() {
-  alert("did something");
-});
